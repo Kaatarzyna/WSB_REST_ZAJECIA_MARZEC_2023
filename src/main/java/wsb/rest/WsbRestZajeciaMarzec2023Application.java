525a779ffac77317c0c1df16d4b4af00711699fc
Katarzyna Duchowska <k.duchowska@o2.pl>
2023-03-11 09:30:51 +0100

    init

---
+++ src/main/java/wsb/rest/WsbRestZajeciaMarzec2023Application.java
@@ -0,0 +1,13 @@
+package wsb.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsbRestZajeciaMarzec2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsbRestZajeciaMarzec2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/wsb/rest/WsbRestZajeciaMarzec2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package wsb.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsbRestZajeciaMarzec2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
